e7e30a54f015afecc11d220ff7566c9824b648f7
steven <stevenle2011@gmail.com>
2017-06-14 15:38:55 +0700

    add compression

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app = express();
 app.use(morgan('dev'));
 var port = process.env.PORT || 3000;
 
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
 
 app.use(express.static(__dirname + '/dist'));
 app.use('/',express.static(__dirname + '/public'));
