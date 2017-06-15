c86209f307f06b8cdcfda8c10ad050afe788477d
steven <stevenle2011@gmail.com>
2017-06-15 11:56:22 +0700

    fix webpack for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -30,5 +30,32 @@ module.exports = {
     query:{presets:['react','es2015']}
 	}
   ]
-}
+},
+  plugins: [
+new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
 };
